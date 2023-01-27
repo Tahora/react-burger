6debf17c16152536b9bb876a5779cbdfb9a8b59b
Tahora <node1@yandex.ru>
2023-01-27 22:43:15 +0400

    страница регистрации

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
 import styles from "./app.module.css";
 import { AppHeader } from "../appHeader/appHeader";
 import { BurgerIngredients } from "../burgerIngredients/burgerIngredients";
@@ -7,6 +8,7 @@ import { getIngredients } from "../../services/actions/ingredients";
 import { useDispatch, useSelector } from "react-redux";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { RegistrationPage} from "../../pages/registration";
 
 export function App() {
   const { ingredients, isLoading, hasError } = useSelector((store) => ({
@@ -25,16 +27,26 @@ export function App() {
     <div className={styles.app}>
       <AppHeader />
       <main className={styles.content}>
-        {isLoading && "Загрузка..."}
-        {hasError && "Произошла ошибка"}
-        {!isLoading && !hasError && ingredients?.length && (
-          <>
-            <DndProvider backend={HTML5Backend}>
-              <BurgerIngredients />
-              <BurgerConstructor />
-            </DndProvider>
-          </>
-        )}
+          <BrowserRouter>
+              <Routes>
+                  <Route  path="/register" element={<RegistrationPage/>}/>
+                  <Route  path="/" element={
+                      <>
+                          {isLoading && "Загрузка..."}
+                          {hasError && "Произошла ошибка"}
+                          {!isLoading && !hasError && ingredients?.length && (
+                              <>
+                                  <DndProvider backend={HTML5Backend}>
+                                      <BurgerIngredients />
+                                      <BurgerConstructor />
+                                  </DndProvider>
+                              </>
+                          )}
+                      </>
+                  }/>
+              </Routes>
+          </BrowserRouter>
+
       </main>
     </div>
   );
---
+++ src/components/registration/registration.js
@@ -0,0 +1,59 @@
+import React from "react";
+import styles from "./registration.module.css";
+import {
+    Input,
+    EmailInput,
+    PasswordInput,
+    Button,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+
+
+export function Registration(props) {
+    return (
+        <div className={`${styles.container}`}>
+            <h1 className="text text_type_main-medium">
+                Регистрация
+            </h1>
+            <Input
+                type={'text'}
+                placeholder={'Имя'}
+                //onChange={e => setValue(e.target.value)}
+                //icon={'CurrencyIcon'}
+                //value={value}
+                name={'name'}
+                error={false}
+                //ref={inputRef}
+                //onIconClick={onIconClick}
+                //errorText={'Ошибка'}
+                size={'default'}
+                extraClass="ml-1"
+            />
+            <EmailInput
+                //onChange={onChange}
+                //value={value}
+                name={'email'}
+                placeholder="E-mail"
+                isIcon={false}
+                //extraClass="mb-2"
+            />
+            <PasswordInput
+                //onChange={onChange}
+                //value={value}
+                name={'password'}
+               // extraClass="mb-2"
+            />
+            <Button
+                htmlType="button"
+                type="primary"
+                size="large"
+                extraClass="pt-4 pb-4 mb-4"
+                //onClick={props.onClick}
+            >Зарегистрироваться
+            </Button>
+            <div className={`${styles.linkContainer} mt-10`}>
+            <p className="text text_type_main-default text_color_inactive">Уже зарегистрированы?</p>
+            <a  href="#" className={`${styles.link} text text_type_main-default`}>Войти</a>
+            </div>
+        </div>
+    );
+}
